3ca5f59e54d88b73a0175950f7482361b55bd6c0
HaffiM <53666594+haffi96@users.noreply.github.com>
2021-09-24 00:51:10 +0100

    Aggregate masts for tenants and introduce code linting (#1)
    * Aggregate masts for each tenant
    
    * Formatted with black
    
    Co-authored-by: Haffi Mazhar <haff@haffis-air.home>

--- src/sorter/sorter.py
+++ src/sorter/sorter.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python3
 import csv
+from pprint import pprint
+from colorama import Fore, Style
+from tabulate import tabulate
 
-data = list(csv.DictReader(open('dataset.csv', 'r')))
-# data = list(csv.reader(open('dataset.csv', 'r')))
+data = list(csv.DictReader(open("dataset.csv", "r")))
 
-# def open_file():
-#     with open('dataset.csv') as csvfile:
-#         csv_reader = csv.reader(csvfile, delimiter=',')
-#     return csv_reader
 
-def sort_rent_dict(data, rent_only=True):
-    tenants = sorted(data, key=lambda k: k['Current Rent'])
+def sort_rent(data, rent_only=True):
+    tenants = sorted(data, key=lambda k: k["Current Rent"])
     if rent_only:
-        rent_only_list = [tenant['Current Rent'] for tenant in tenants[:5]]
+        rent_only_list = [tenant["Current Rent"] for tenant in tenants[:5]]
         print(rent_only_list)
     else:
         print(tenants[:5])
-    
-def long_tenancies(data, tenancy_time='25'):
-    long_tenant_list = [tenant for tenant in data if tenant['Lease Years'] == tenancy_time]
-    print(long_tenant_list)
-    all_rents = [rent['Current Rent'] for rent in long_tenant_list]
+
+
+def long_tenancies(data, tenancy_time="25", print_table=False):
+    long_tenant_list = [
+        tenant for tenant in data if tenant["Lease Years"] == tenancy_time
+    ]
+    if print_table:
+        header = data[0].keys()
+        rows = [x.values() for x in data]
+        print(tabulate(rows, header, tablefmt="grid", numalign="center"))
+    pprint(long_tenant_list, sort_dicts=False)
+    all_rents = [rent["Current Rent"] for rent in long_tenant_list]
     total_rent = sum([int(float(x)) for x in all_rents])
-    print(total_rent)
+    print(f"{Fore.RED}TOTAL RENT: {total_rent}{Style.RESET_ALL}")
+
+
+def aggregate_tenant_masts(data):
+    tenant_list = [tenant["Tenant Name"] for tenant in data]
+    aggregated = dict.fromkeys(tenant_list)
+    for k in aggregated.keys():
+        total_masts = sum(x.get("Tenant Name") == k for x in data)
+        aggregated.update({k: total_masts})
+    pprint(aggregated)
 
 
 if __name__ == "__main__":
-    sort_rent_dict(data)
+    sort_rent(data)
     long_tenancies(data)
+    aggregate_tenant_masts(data)
